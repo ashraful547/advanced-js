37daf1aa140cdbe4949c7a38d113270fd92269bd
ashraful547 <alfatulk@gmail.com>
2021-02-02 14:10:15 +0600

    nullvsundefined

---
+++ nullvsundefined.js
@@ -0,0 +1,24 @@
+
+
+function add(num1, num2){
+console.log(num1 + num2);
+return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name: "smart dude", phone : 022994};
+console.log(premik.gf);
+
+let pakhi; 
+console.log(pakhi);
+
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [2,4,5]
+console.log(ages[11])
